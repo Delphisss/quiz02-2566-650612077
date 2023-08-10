cfecde9fc4b59bfffa3e2602bacf5eefba4a553f
Delphisss <Kong_led_@hotmail.com>
2023-08-10 14:59:59 +0700

    T^T

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import Reply from "./Reply";
+
 export const Comment = ({
   userImagePath,
   username,
@@ -5,10 +8,26 @@ export const Comment = ({
   likeNum,
   replies,
 }) => {
+  const showLikes = likeNum > 0;
   return (
-    <div>
-      your code here...
-      {/* map-loop render Reply component here */}
+    <div className="comment">
+      <img src={userImagePath} alt={`${username}'s profile`} />
+      <div className="comment-details">
+        <h3>{username}</h3>
+        <p>{commentText}</p>
+        {showLikes && <p>Likes: {likeNum}</p>}
+        <div className="replies">
+          {replies.map((reply, index) => (
+            <Reply
+              key={index}
+              userImagePath={reply.userImagePath}
+              username={reply.username}
+              replyText={reply.commentText}
+              likeNum={reply.likeNum}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -1,3 +1,16 @@
-export const Reply = ({ userImagePath, username, replyText, likeNum }) => {
-  return <div>your code here...</div>;
+import React from "react";
+
+const Reply = ({ userImagePath, username, replyText, likeNum }) => {
+  const showLikes = likeNum > 0;
+
+  return (
+    <div className="reply">
+      <img src={userImagePath} alt={`${username}'s profile`} />
+      <div className="reply-details">
+        <h4>{username}</h4>
+        <p>{replyText}</p>
+        {showLikes && <p>Likes: {likeNum}</p>}
+      </div>
+    </div>
+  );
 };
